c1682057efe013cc5131bb158f8be4646d982c9c
SteeleCD <steelecd2110@hotmail.com>
2016-07-26 09:31:34 +0100

    updated folders for legion

--- R/assignFolders.R
+++ R/assignFolders.R
@@ -1,23 +1,34 @@
 assignFolders = function()
 	{
-		if(Sys.info()['sysname']=='Darwin')
+	user = Sys.info()['user']
+	if(user=='c.steele')
 		{
+		# MAC
 		patholDir='/Volumes/pathology$/'
 		ucbtcdsDir='/Volumes/ucbtcdsh$/'
 		commonDir='/Volumes/common$/'
 		grpDir='/Volumes/grp11$/CI_Pathology_Steele/'
 		dropDir='/Users/c.steele/Dropbox/'
-		} else {
+		} else if(user=='chris') {
+		# WORK
 		patholDir='/media/pathology/'
 		ucbtcdsDir='/media/ucbtcds/'
 		commonDir='/media/common/'
 		grpDir='/media/grp11/CI_Pathology_Steele/'
 		dropDir='/home/chris/Dropbox/'
+		} else {
+		# LEGION
+		scratchDir='/scratch/scratch/ucbtcds'
 		}
-	return(list(patholDir=patholDir,
+	if(user%in%c('c.steele','chris'))
+		{
+		return(list(patholDir=patholDir,
 			ucbtcdsDir=ucbtcdsDir,
 			commonDir=commonDir,
 			grpDir=grpDir,
 			dropDir=dropDir))
+		} else {
+		return(list(scratchDir=scratchDir))
+		}
 	}
 
