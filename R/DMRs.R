2ab085cd8396cb95f62c4d7cdd84387492b70d08
SteeleCD <steelecd2110@hotmail.com>
2016-07-28 09:34:38 +0100

    Added overlapping genes to DMR plots

--- R/DMRs.R
+++ R/DMRs.R
@@ -1,3 +1,32 @@
+# function to get genes that overlap a region
+getOverlapGenes = function(chr,start,end)
+	{
+	# get gRanges from this DMR
+	ranges = gsub(" ","",paste0(chr,":",start,":",end))
+	library(dplyr)
+	gRanges = sapply(ranges, function (x) {res=strsplit(x, ':')}) %>%
+		unlist %>%
+		as.numeric %>%
+		matrix(ncol=3, byrow=T) %>%
+		as.data.frame %>%
+		dplyr::select(chrom=V1, start=V2, end=V3) %>%
+		mutate(chrom=paste0('chr', chrom)) %>%
+		makeGRangesFromDataFrame
+	# get Hsapiens genes
+	library(Homo.sapiens)
+	genesRanges = genes(TxDb.Hsapiens.UCSC.hg19.knownGene)
+	# overlaps between genes and DMR
+	overlap = subsetByOverlaps(genesRanges,gRanges); overlap
+	geneStart = overlap@ranges@start
+	geneEnd = overlap@ranges@start+overlap@ranges@width
+	# get gene names
+	genes = as.data.frame(org.Hs.egSYMBOL)
+	DMRgenes = genes[which(genes$gene_id%in%overlap$gene_id),]$symbol
+	# return
+	return(list(geneStart=geneStart,geneEnd=geneEnd,geneName=DMRgenes))
+	}
+
+# function to get beta values of all probes within a region, and any genes that overlap the region
 getRegion = function(betas,chr,start,end,manifest,flank=10000)
 	{
 	region = sort(c(start,end))
@@ -17,9 +46,13 @@ getRegion = function(betas,chr,start,end,manifest,flank=10000)
 	info = manifest$MAPINFO
 	names(info) = manifest$IlmnID
 	info = info[which(names(info)%in%rownames(betas))]
-	return(list(betas=betas,pos=info))
+	# get overlapping genes
+	overlapGenes = getOverlapGenes(chr,region[1],region[2])
+	# return
+	return(list(betas=betas,pos=info,overlaps=overlapGenes))
 	}
 
+# function to plot DMRs with overlapping genes displayed (if any)
 plotDMR = function(betas,dmrs,index,manifest,flank=10000,groupIndices,doInvLogit=TRUE)
 	{
 	# get values in region
@@ -29,11 +62,36 @@ plotDMR = function(betas,dmrs,index,manifest,flank=10000,groupIndices,doInvLogit
 	pos2 = matrix(region$pos,ncol=length(groupIndices[[2]]),nrow=nrow(region$betas))
 	# inverse logit for Ms
 	if(doInvLogit) region$betas = invlogit(region$betas)
-	# plot
-	plot(pos1,region$betas[,groupIndices[[1]]],ylim=range(region$betas),xlab="Position",ylab=ifelse(doInvLogit,"Beta","M"),main=paste0("Chromosome ",dmrs[index,"chr"]),col="blue")
+	# layout
+	if(length(region$overlaps$geneStart)>0) 
+		{
+		layout(matrix(1:2,ncol=1,nrow=2),widths=1,heights=c(3,1))
+		par(mar=c(0,4,4,2))
+		XLAB = NA
+		XAXT = 'n'
+		} else {
+		par(mar=c(5,4,4,2))
+		layout(matrix(1,ncol=1,nrow=1),widths=1,heights=c(1))
+		XLAB = "Position"
+		XAXT = NULL
+		}
+	# plot DMR
+	plot(pos1,region$betas[,groupIndices[[1]]],ylim=range(region$betas),xlab=XLAB,ylab=ifelse(doInvLogit,"Beta","M"),main=paste0("Chromosome ",dmrs[index,"chr"]),col="blue",xaxt=XAXT)
 	points(pos2,region$betas[,groupIndices[[2]]],col="red")
 	abline(v=c(dmrs[index,"end"],dmrs[index,"start"]),lty=2)
 	toOrder = order(pos1[,1])
 	lines(pos1[toOrder,1],rowMeans(region$betas[,groupIndices[[1]]])[toOrder],col="blue",lwd=2)
 	lines(pos2[toOrder,1],rowMeans(region$betas[,groupIndices[[2]]])[toOrder],col="red",lwd=2)
+	# plot genes
+	if(length(region$overlaps$geneStart)>0)
+		{
+		par(mar=c(5,4,0,2))
+		yVals = 0:length(region$overlaps$geneStart)
+		plot(NA,xlim=range(pos1),ylim=yVals,yaxt="n",ylab=NA,xlab="Position")
+		for(i in 1:length(yVals))
+			{
+			polygon(c(region$overlaps$geneStart[i],region$overlaps$geneStart[i],region$overlaps$geneEnd[i],region$overlaps$geneEnd[i]),c(yVals[i]+0.25,yVals[i]+0.75,yVals[i]+0.75,yVals[i]+0.25),col="black")
+			text(x=region$overlaps$geneEnd[i]+500,y=yVals[i]+0.5,region$overlaps$geneName[i],cex=1.5)
+			}
+		}
 	}
