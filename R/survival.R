5233f9a86a86764242bbcf5710efb65854920f91
SteeleCD <steelecd2110@hotmail.com>
2017-10-31 16:03:43 +0000

    Added plotting AFT

--- R/survival.R
+++ R/survival.R
@@ -15,19 +15,12 @@ genNewData = function(x,nRep,whichKeep=NULL)
 		}
 	}
 
-# simulate new data from cox model and plot
-plotCox = function(coxPH,data,whichPlot,whichKeep=NULL,outFile=NULL,colour=NULL)
+# Generate new data for survival prediction
+genNewDataAll = function(data,whichPlot,whichKeep=NULL)
 	{
 	# how many levels
 	facLevels = levels(data[,whichPlot])
 	nRep = length(facLevels)
-	# get new data frame, keeping other variables constant
-	#newData = sapply(data[,-grep(whichPlot,colnames(data))],FUN=function(x)
-	#	if(is.factor(x)) return(rep(levels(x)[1],nRep)) else return(rep(mean(x,na.rm=TRUE),nRep))
-	#	)
-	#newData = cbind(newData,facLevels)
-	#colnames(newData)[ncol(newData)] = whichPlot
-	#newData = as.data.frame(newData)
 	newData = data.frame(factor(facLevels,levels=facLevels))
 	for(i in colnames(data))
 		{
@@ -40,6 +33,13 @@ plotCox = function(coxPH,data,whichPlot,whichKeep=NULL,outFile=NULL,colour=NULL)
 			}
 		}
 	colnames(newData)[1] = whichPlot
+	return(newData)
+	}
+
+# simulate new data from cox model and plot
+plotCox = function(coxPH,data,whichPlot,whichKeep=NULL,outFile=NULL,colour=NULL)
+	{
+	newData = genNewDataAll(data,whichPlot,whichKeep)
 	# fit model again with new data
 	fit <- survfit(coxPH, newdata = newData)
 	# plot simulated data
@@ -52,4 +52,27 @@ plotCox = function(coxPH,data,whichPlot,whichKeep=NULL,outFile=NULL,colour=NULL)
 	return(newData)
 	}
 
+
+
+# simulate new data from AFT flexsurv model, and plot
+plotAFT = function(object,whichPlot,data,times,whichKeep=NULL,log=TRUE,colour,legloc="bottomleft",...)
+	{
+	# simulate new data, then get survival probs
+	info = summary(object,newdata=genNewDataAll(data,whichPlot,whichKeep=whichKeep))
+	# get covariate names
+	infoNames = sapply(names(info),FUN=function(x) strsplit(x,split=", ")[[1]],simplify=FALSE)
+	infoNames = sapply(sapply(infoNames,FUN=function(x) x[grep(whichPlot,x)]),FUN=function(y) strsplit(y,split="=")[[1]][2])
+	if(log)
+		{
+		# plot time on log scale
+		plot(NA,xlim=range(log(times)),ylim=0:1,...)
+		sapply(1:length(info),FUN=function(x) lines(info[[x]][,"time"],info[[x]][,"est"],col=colour[x],...))
+		} else {
+		# plot time on ordinary scale
+		plot(NA,xlim=range(times),ylim=0:1,...)
+		sapply(1:length(info),FUN=function(x) lines(exp(info[[x]][,"time"]),info[[x]][,"est"],col=colour[x],...))
+		}
+	# add legend
+	legend(x=legloc,legend=infoNames,col=colour,lty=1,lwd=2)
+	}
 
